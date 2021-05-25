806d1b7a0540ec55eb478cd08c1dd2f760456b2a
doramatadora <hello@doramilitaru.com>
2021-05-25 13:09:09 +0100

    add demo tooling

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use fastly::http::{header, StatusCode};
+use fastly::{Error, Request, Response};
+
+const BACKEND_NAME: &str = "github";
+
+#[fastly::main]
+fn main(mut req: Request) -> Result<Response, Error> {
+    // Make any desired changes to the client request.
+    req.set_header(header::HOST, "doramatadora.github.io");
+    req.set_pass(true);
+    req.set_path(&format!("/hackadora{}", &req.get_url().path()));
+    // Pattern match on the path.
+    match req.get_url().path() {
+        "/hackadora/.well-known/fastly/demo-manifest" => Ok(Response::from_status(StatusCode::OK)
+            .with_body_text_plain(include_str!("./demo-manifest"))),
+        _ => Ok(req.send(BACKEND_NAME)?),
+    }
+}
